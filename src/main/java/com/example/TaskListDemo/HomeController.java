848b4f28933fdc6cab98dfa0b5d1700dc289588f
mskhrk7101 <msk19970709@gmail.com>
2022-06-19 19:44:33 +0900

    時刻HYML表示

--- src/main/java/com/example/TaskListDemo/HomeController.java
+++ src/main/java/com/example/TaskListDemo/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.TaskListDemo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -9,18 +10,21 @@ import java.time.LocalDateTime;
 @Controller
 public class HomeController {
     @RequestMapping("/hello")
-    @ResponseBody
-    String hello() {
-        return """
-                <html>
-                    <head><title>Hello</title></head>
-                    <body>
-                        <h1>Hello</h1>
-                        It works!<br>
-                        現在時刻は%sです。
-                    </body>
-                </html>
-                """.formatted(LocalDateTime.now());
+//    @ResponseBody
+//    String hello() {
+//        return """
+//                <html>
+//                    <head><title>Hello</title></head>
+//                    <body>
+//                        <h1>Hello</h1>
+//                        It works!<br>
+//                        現在時刻は%sです。
+//                    </body>
+//                </html>
+//                """.formatted(LocalDateTime.now());
+    String hello(Model model){
+        model.addAttribute("time", LocalDateTime.now());
+        return "hello";
     }
 
 }
