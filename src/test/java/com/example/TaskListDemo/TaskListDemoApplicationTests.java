90e7081229da9d5df64e054b09983b70df763e84
mskhrk7101 <msk19970709@gmail.com>
2022-06-19 16:54:16 +0900

    First Commit

---
+++ src/test/java/com/example/TaskListDemo/TaskListDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TaskListDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskListDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
