aee6566cc2f5729276be532ff9789d27983e69b1
mskhrk7101 <msk19970709@gmail.com>
2022-06-19 18:36:38 +0900

    Merge remote-tracking branch 'origin/main'

--- src/main/java/com/example/TaskListDemo/TaskListDemoApplication.java
+++ src/main/java/com/example/TaskListDemo/TaskListDemoApplication.java
@@ -3,14 +3,13 @@ package com.example.TaskListDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.example.TaskListDemo.TaskListDemoApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class TaskListDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskListDemoApplication.class, args);
-	}
+	public static void main(String[] args) {SpringApplication.run(TaskListDemoApplication.class, args);}
 	@RequestMapping("/aaaa")
 	String hello(){
 		return "AAA AA";
